f58f3d8c41be1cf41ae78117211ea949ead0a30b
fulviusguelfi <fulvius@gpmail.com.br>
2022-02-25 02:05:57 -0300

    Add CORS configuration

---
+++ src/main/java/com/cfg/bm/data/api/configuration/CorsConfiguration.java
@@ -0,0 +1,16 @@
+package com.cfg.bm.data.api.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+
+@Configuration
+public class CorsConfiguration implements WebFluxConfigurer {
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**").allowedOrigins("*.herokuapp.com").allowedMethods("GET", "POST", "PUT", "DELETE",
+				"OPTIONS", "HEAD", "TRACE", "CONNECT");
+	}
+
+}
