4270503d6f5589ff8e3412421e4dee05e0bd08ef
fulviusguelfi <fulvius@gpmail.com.br>
2022-03-18 19:52:53 -0300

    .

--- src/main/java/com/cfg/bm/data/api/configuration/CorsConfiguration.java
+++ src/main/java/com/cfg/bm/data/api/configuration/CorsConfiguration.java
@@ -10,7 +10,7 @@ public class CorsConfiguration implements WebFluxConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
-			.allowedOrigins("*")
+			.allowedOriginPatterns("*")
 			.allowedMethods("GET", "POST", "PUT", "DELETE","OPTIONS", "HEAD", "TRACE", "CONNECT")
 			.allowCredentials(true)
 			.allowedHeaders("*")
